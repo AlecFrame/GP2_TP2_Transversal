674653c1074b9a7d4200b0a9dfd0c299a30e2d2d
AlecFrame <walteravertacnik@sanluis.edu.ar>
2024-09-30 17:47:46 -0300

    Agregando JFrames
    Agregando JFrames e InternalFrames en clase

--- src/Vista/main.java
+++ src/Vista/main.java
@@ -4,6 +4,10 @@ package Vista;
 public class main {
 
     public static void main(String[] args) {
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        
+        ventana.setVisible(true);
+        
         
     }
     
